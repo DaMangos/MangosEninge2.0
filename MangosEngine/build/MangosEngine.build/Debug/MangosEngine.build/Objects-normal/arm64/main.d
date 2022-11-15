2278b54a7266498407e23197eabc8f32759f06b9
Oliver Horrigan-Pierre <100730946+DaMangos@users.noreply.github.com>
2022-11-15 23:56:58 +0000

    Delete MangosEngine directory

--- MangosEngine/build/MangosEngine.build/Debug/MangosEngine.build/Objects-normal/arm64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/oliverhorriganpierre/Documents/Xcode/MangosEngine/MangosEngine/main.cpp \
-  /Users/oliverhorriganpierre/Documents/Xcode/MangosEngine/MangosEngine/Maths/Algebra.hpp
